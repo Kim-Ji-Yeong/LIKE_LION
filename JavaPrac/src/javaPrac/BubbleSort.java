10becd4647b4740e54d277369bcea2664c6093fb
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-03 23:54:48 +0900

    BubbleSort

--- JavaPrac/src/javaPrac/BubbleSort.java
+++ JavaPrac/src/javaPrac/BubbleSort.java
@@ -13,7 +13,7 @@ public class BubbleSort {
 			arr[i] = sc.nextInt();
 		}
 
-		for (int i = 0; i <arr.length-1; i++) {
+		for (int i = 0; i <n-1; i++) {
 			for (int j = 0; j < n -i- 1; j++) {
 				if (arr[i] > arr[i+1]) {
 					int temp = arr[i + 1];
