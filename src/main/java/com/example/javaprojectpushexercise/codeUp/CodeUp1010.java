c8c9d435c96616d755cbd719615133784ba9e6ac
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-04 20:18:59 +0900

    221004 CodeUp 과제

---
+++ src/main/java/com/example/javaprojectpushexercise/codeUp/CodeUp1010.java
@@ -0,0 +1,10 @@
+package com.example.javaprojectpushexercise.codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1010 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(sc.nextInt());
+    }
+}
