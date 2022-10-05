436edcbce2ba3bf08933bddb3bc990417f2ba8b5
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-05 11:25:40 +0900

    221005

--- src/main/java/com/example/javaprojectpushexercise/Calculator.java
+++ src/main/java/com/example/javaprojectpushexercise/Calculator.java
@@ -1,18 +1,46 @@
 package com.example.javaprojectpushexercise;
 
 public class Calculator {
+
+    private int a;
+    private int b;
+
     //ctrl + e 소스코드 파일 히스토리
-    public void plus(int first,int second){
-        System.out.println(first + second);
+
+    //alt + insert 생성자 추가
+
+
+    public Calculator(int a,int b) {
+        this.a = a;
+        this.b = b;
     }
-    public void minus(int first,int second){
-        System.out.println(first - second);
+
+    public int plus(){
+        return a+b;
     }
-    public void multiple(int first,int second){
-        System.out.println(first * second);
+
+    public int minus(){
+        return a-b;
     }
-    public void divide(int first,int second){
-        float result = (float)first/second;
-        System.out.println(result);
+
+    public int multiple(){
+        return a*b;
     }
+
+    public float divide(){
+        return (float) a / b;
+    }
+//    public void plus(int first, int second){
+//        System.out.println(first + second);
+//    }
+//    public void minus(int first,int second){
+//        System.out.println(first - second);
+//    }
+//    public void multiple(int first,int second){
+//        System.out.println(first * second);
+//    }
+//    public void divide(int first,int second){
+//        float result = (float)first/second;
+//        System.out.println(result);
+//    }
 }
