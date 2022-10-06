fabcf450e392d6c53a10808ea7044728ed5b50f9
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-06 09:24:16 +0900

    221006 List

---
+++ src/main/java/com/example/javaprojectpushexercise/Day3/collection/ListExercise.java
@@ -0,0 +1,20 @@
+package com.example.javaprojectpushexercise.Day3.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    private List<String> students; //private로 해야함
+
+    public ListExercise(){
+        this.students = new ArrayList<>(); //생성자에서 new해도됨 생성자가 제일 먼저 만들어지니
+        this.students.add("김지영");
+        this.students.add("최우식");
+        this.students.add("박지민");
+        this.students.add("차은우");
+    }
+
+    public List<String> getStudents(){
+        return this.students;
+    }
+}
