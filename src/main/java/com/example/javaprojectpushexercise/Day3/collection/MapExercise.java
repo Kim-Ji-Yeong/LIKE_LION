3c428f077ca94d3f2518eea365167fa19ed75ca7
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-06 11:21:23 +0900

    221006

--- src/main/java/com/example/javaprojectpushexercise/Day3/collection/MapExercise.java
+++ src/main/java/com/example/javaprojectpushexercise/Day3/collection/MapExercise.java
@@ -48,6 +48,7 @@ public class MapExercise {
         System.out.println(map.get("권아윤"));
         System.out.println(map.get("박도아"));
         System.out.println(map.get("유서이"));
+        System.out.println(map.get("남혜준"));
 
     }
 }
