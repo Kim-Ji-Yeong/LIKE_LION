7aef1bfceb52c257c9511744b81033ce6a2d57c6
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-17 09:11:31 +0900

    별찍기

---
+++ src/main/java/com/example/likelion/RightTriangle.java
@@ -0,0 +1,12 @@
+package com.example.likelion;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
