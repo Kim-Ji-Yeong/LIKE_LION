cd15cfc5cd97e2508569fef25ca3f692b481dd06
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-24 19:19:05 +0900

    Initial commit

---
+++ src/test/java/com/example/like_lion2/LikeLion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.like_lion2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLion2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
