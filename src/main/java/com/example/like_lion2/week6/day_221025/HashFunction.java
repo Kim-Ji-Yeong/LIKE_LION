6c08da9f398824669c1661696b3dda16d5bd489e
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-25 10:24:27 +0900

    Hash

--- src/main/java/com/example/like_lion2/week6/day_221025/HashFunction.java
+++ src/main/java/com/example/like_lion2/week6/day_221025/HashFunction.java
@@ -3,14 +3,14 @@ package com.example.like_lion2.week6.day_221025;
 public class HashFunction {
     public int hash(String key) {
         int asciiSum = 0;
-        for (int i = 0; i < key.length(); i++) {
+        for(int i=0; i<key.length(); i++) {
             asciiSum += key.charAt(i);
         }
-        return asciiSum % 1000;    // 1000개
+        return asciiSum % 90;
     }
 
     public static void main(String[] args) {
         HashFunction hf = new HashFunction();
-        System.out.println(hf.hash("TOEIC"));
+        hf.hash("yeong");
     }
 }
