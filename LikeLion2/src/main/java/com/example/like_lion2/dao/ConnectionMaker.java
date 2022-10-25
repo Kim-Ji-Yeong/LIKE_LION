da39a260a1dbae5903ef55b346c5a760d833e107
Kim-Ji-Yeong <yeong6415@daum.net>
2022-10-26 01:06:30 +0900

    Delete LikeLion2 directory

--- LikeLion2/src/main/java/com/example/like_lion2/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.example.like_lion2.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    Connection makeConnection() throws SQLException;
-}
